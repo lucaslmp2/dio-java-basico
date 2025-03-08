cdbfb3361f6e9a10906a7aa70f473b3536493c12
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-03-08 11:07:53 -0300

    Atualizando CaixaEletronico

--- dio-java-basic/Estudos/Lucas/QuintoDia/ControleFluxo/src/CaixaEletronico.java
+++ dio-java-basic/Estudos/Lucas/QuintoDia/ControleFluxo/src/CaixaEletronico.java
@@ -1,7 +1,7 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 27.0;
-        double valorSolicitado = 15.0;
+        double valorSolicitado = 30.0;
 
         if (valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
@@ -9,6 +9,7 @@ public class CaixaEletronico {
             System.out.println("Saldo atual: " + saldo);
         } else {
             System.out.println("Saldo insuficiente!");
+            System.out.println("Saldo atual: " + saldo);
         } 
     }
 }
