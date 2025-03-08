bb03407f67deeb564191587d61475326aa1a93ab
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-03-08 10:35:55 -0300

    adicionando arquivo pom.xml

--- dio-java-basic/Estudos/Lucas/TerceiroDia/SobreMim.java
+++ dio-java-basic/Estudos/Lucas/TerceiroDia/SobreMim.java
@@ -1,20 +1,20 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class SobreMim {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Qual é o seu nome?: ");
-            String nome = scanner.nextLine();
-            System.out.println("Qual é o seu sobrenome?: ");
-            String sobrenome = scanner.nextLine();
-            System.out.println("Qual é a sua idade?: ");
-            int idade = scanner.nextInt();
-            System.out.println("Qual é a sua altura?: ");
-            float altura = scanner.nextFloat();
-            System.out.println("Qual é o seu peso?: ");
-            float peso = scanner.nextFloat();
-            System.out.println("Olá, meu nome é " + nome + " " + sobrenome + ", tenho " + idade + " anos, minha altura é " + altura + "m e peso " + peso + "kg.");
-            // Fechar o scanner para evitar vazamento de recursos
-        }
+        Scanner ler = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Qual é o seu nome?: ");
+        String nome = ler.nextLine();
+        System.out.println("Qual é o seu sobrenome?: ");
+        String sobrenome = ler.nextLine();
+        System.out.println("Qual é a sua idade?: ");
+        int idade = ler.nextInt();
+        System.out.println("Qual é a sua altura?: ");
+        float altura = ler.nextFloat();
+        System.out.println("Qual é o seu peso?: ");
+        float peso = ler.nextFloat();
+        System.out.println("Olá, meu nome é " + nome + " " + sobrenome + ", tenho " + idade + " anos, minha altura é " + altura + "m e peso " + peso + "kg.");
+        ler.close();
     }
 }
