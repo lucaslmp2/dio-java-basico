7ac1c2be91e0d8abb4e429e6979d5289f71a793c
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-03-08 12:05:32 -0300

    Finalizando estruturas condicionais basica

--- dio-java-basic/Estudos/Lucas/QuintoDia/ControleFluxo/src/ResultadoEscolar.java
+++ dio-java-basic/Estudos/Lucas/QuintoDia/ControleFluxo/src/ResultadoEscolar.java
@@ -2,9 +2,15 @@ public class ResultadoEscolar {
     /*Avaliando estruturas condicionais e ultilizando debug para acompanhamento de breankingpoints */
     public static void main(String[] args) {
         int nota = 6;
-
+        /*Condição Ternária */
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+        
         if(nota >= 7){
             System.out.println("Aprovado!");
+
+        }else if(nota >= 5 && nota < 7){/*Demonstrando condicional encadeada */
+            System.out.println("Recuperação!");
         } else {
             System.out.println("Reprovado!");
         }
---
+++ dio-java-basic/Estudos/Lucas/QuintoDia/ControleFluxo/src/SistemaMedida.java
@@ -0,0 +1,22 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "A";
+        //switch case primeira demonstração
+        switch (sigla) {
+            case "P":{
+                System.out.println("Pequeno");
+                break;
+            }
+            case "M":{
+                System.out.println("Médio");
+                break;
+            }
+            case "G":{
+                System.out.println("Grande");
+                break;
+            }
+            default:
+                System.out.println("Indefinido");        
+            }
+    }
+}
