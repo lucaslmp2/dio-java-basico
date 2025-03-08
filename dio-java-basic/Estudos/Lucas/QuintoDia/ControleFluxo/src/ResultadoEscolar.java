02dec491d11c02bc1edfc548699c22d960748e17
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-03-08 11:21:02 -0300

    corrigindo condição de aprovação no ResultadoEscolar

--- dio-java-basic/Estudos/Lucas/QuintoDia/ControleFluxo/src/ResultadoEscolar.java
+++ dio-java-basic/Estudos/Lucas/QuintoDia/ControleFluxo/src/ResultadoEscolar.java
@@ -3,7 +3,7 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        if(nota > 7){
+        if(nota >= 7){
             System.out.println("Aprovado!");
         } else {
             System.out.println("Reprovado!");
