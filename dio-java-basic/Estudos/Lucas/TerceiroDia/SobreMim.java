f9f914604beb300bd353987eda120d6ac156940b
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-08-21 13:12:05 -0300

    Trabalhando com exceções Try/Catch

--- dio-java-basic/Estudos/Lucas/TerceiroDia/SobreMim.java
+++ dio-java-basic/Estudos/Lucas/TerceiroDia/SobreMim.java
@@ -1,20 +1,28 @@
+package TerceiroDia;
+
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class SobreMim {
     public static void main(String[] args) throws Exception {
-        Scanner ler = new Scanner(System.in).useLocale(Locale.US);
-        System.out.println("Qual é o seu nome?: ");
-        String nome = ler.nextLine();
-        System.out.println("Qual é o seu sobrenome?: ");
-        String sobrenome = ler.nextLine();
-        System.out.println("Qual é a sua idade?: ");
-        int idade = ler.nextInt();
-        System.out.println("Qual é a sua altura?: ");
-        float altura = ler.nextFloat();
-        System.out.println("Qual é o seu peso?: ");
-        float peso = ler.nextFloat();
-        System.out.println("Olá, meu nome é " + nome + " " + sobrenome + ", tenho " + idade + " anos, minha altura é " + altura + "m e peso " + peso + "kg.");
-        ler.close();
+        try{
+            Scanner ler = new Scanner(System.in).useLocale(Locale.US);
+            System.out.println("Qual é o seu nome?: ");
+            String nome = ler.nextLine();
+            System.out.println("Qual é o seu sobrenome?: ");
+            String sobrenome = ler.nextLine();
+            System.out.println("Qual é a sua idade?: ");
+            int idade = ler.nextInt();
+            System.out.println("Qual é a sua altura?: ");
+            float altura = ler.nextFloat();
+            System.out.println("Qual é o seu peso?: ");
+            float peso = ler.nextFloat();
+            System.out.println("Olá, meu nome é " + nome + " " + sobrenome + ", tenho " + idade + " anos, minha altura é " + altura + "m e peso " + peso + "kg.");
+            ler.close();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Os campos de Idade e Altura devem ser números.");
+        }
     }
 }
---
+++ target/classes/QuintoDia/ControleFluxo/README.md
@@ -0,0 +1,18 @@
+## Introdução
+
+Bem-vindo ao mundo Java no VS Code. Aqui está um guia para ajudá-lo a começar a escrever código Java no Visual Studio Code.
+
+## Estrutura de Pastas
+
+O workspace contém duas pastas por padrão:
+
+- `src`: pasta para manter os arquivos fonte
+- `lib`: pasta para manter as dependências
+
+Os arquivos compilados serão gerados na pasta `bin` por padrão.
+
+> Se você quiser personalizar a estrutura de pastas, abra `.vscode/settings.json` e atualize as configurações relacionadas.
+
+## Gerenciamento de Dependências
+
+A visualização `JAVA PROJECTS` permite gerenciar suas dependências. Mais detalhes podem ser encontrados [aqui](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
