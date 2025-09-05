2a6dc20cf0148b0bf2ea190c4ebc8d56c8dcbb79
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-09-05 17:21:13 -0300

    desafio 2

---
+++ dio-java-basic/Estudos/Lucas/DecimaDia/desafio2.java
@@ -0,0 +1,23 @@
+package DecimaDia;
+
+import java.util.Scanner;
+public class desafio2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+
+        // TODO: Implemente a classificação do cliente com base no saldo:
+        if(saldo<0){
+          System.out.println("Negativado");
+        }else if(saldo==0){
+          System.out.println("Baixo");
+        }else if(saldo > 500){
+          System.out.println("Confortavel");
+        }
+
+
+        scanner.close();
+    }
+}
+
